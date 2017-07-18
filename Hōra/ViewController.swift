b05e9dbe99af9d083ec1f4bb6c657115e88e90e5
Gun Cho <guncho@guns-macbook-pro-2.local>
2017-07-18 15:39:38 -0700

    Initial Commit

---
+++ Hōra/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hōra
+//
+//  Created by Gun Cho on 7/18/17.
+//  Copyright © 2017 Gun Cho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
